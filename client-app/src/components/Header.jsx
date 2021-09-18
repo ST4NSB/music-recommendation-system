d2140c9a0e3aea12602111b6271a8cdc23f116f5
ST4NSB <barbulescuadrian10@gmail.com>
2021-09-18 19:45:15 +0300

    added logo + some minor style changes

--- client-app/src/components/Header.jsx
+++ client-app/src/components/Header.jsx
@@ -4,6 +4,7 @@ import SearchBar from "./SearchBar";
 import { getSongsApi } from "../utils/apiRequests";
 import { getSearchResults } from "../actions/searchResults.actions";
 import { useSelector } from "react-redux";
+import logo from "../images/logo.png";
 
 const Header = () => {
     const { searchText, likedSongs } = useSelector(state => state);
@@ -30,22 +31,25 @@ const Header = () => {
 
     const showRecommendedLink = () => {
         if (location.pathname !== '/recommendations')
-            return (<Link to='/recommendations'>GET RECOMMENDED SONG</Link>);
+            return (<Link to='/recommendations'>Get Recommendations</Link>);
     }
 
     return (
-        <header className="bg-milk border-b md:flex md:items-center md:justify-between p-4 pb-0 shadow-lg md:pb-4">
+        <header className="sticky top-0 z-50 bg-milk border-b md:flex md:items-center md:justify-between p-4 pb-0 shadow-lg md:pb-4">
             <div className="flex items-center justify-between mb-4 md:mb-0">
                 <h1 className="leading-none text-xl text-grey-darkest">
                     <a className="no-underline text-grey-darkest hover:text-black" href="#">
-                        <img src="" 
-                            alt="header logo" />
+                        <img src={logo} 
+                             className="h-22 w-32"
+                             alt="header logo" />
                     </a>
                 </h1>
             </div>
          
+         <div>
             {showRecommendedLink()}
             {showSearchBar()}
+        </div>
          
         </header>
     );
--- client-app/src/components/Main.jsx
+++ client-app/src/components/Main.jsx
@@ -1,7 +1,7 @@
 
 const Main = ({children}) => {
     return (
-        <main className="bg-milk min-h-screen">
+        <main className="bg-yellow-50 min-h-screen pt-3">
             {children}
         </main>
     );    
