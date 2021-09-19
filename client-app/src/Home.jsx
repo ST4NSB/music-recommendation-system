757b0b80e0966b58f9530f69bd13deab3965c204
ST4NSB <barbulescuadrian10@gmail.com>
2021-09-19 20:30:48 +0300

    added css classes to wrapper div in Home

--- client-app/src/Home.jsx
+++ client-app/src/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
         <>
             <Header />
             <Main>
-                <h1 className="flex flex-col justify-center items-center">Music Recommender System</h1>
-                <p className="flex flex-col justify-center items-center">Information to be added here ....</p>
                 <div className="flex flex-col justify-center items-center">
+                    <h1>Music Recommender System</h1>
+                    <p>Information to be added here ....</p>
                     <Link className="px-4 py-2 rounded-md text-sm font-medium border shadow focus:outline-none focus:ring transition text-gray-600 bg-gray-50 border-gray-200 hover:bg-gray-100 active:bg-gray-200 focus:ring-gray-300" 
                             to='/search'>
                             Start discovering
