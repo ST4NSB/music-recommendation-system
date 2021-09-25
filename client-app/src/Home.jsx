d7dd752b0558abd8e5af9979ac5657a3e173ab43
ST4NSB <barbulescuadrian10@gmail.com>
2021-09-26 00:23:21 +0300

    small changes for text tags

--- client-app/src/Home.jsx
+++ client-app/src/Home.jsx
@@ -19,23 +19,24 @@ const Home = () => {
             <Main>
                 <div className="flex flex-col justify-center items-center pt-12">
                     <h1 className="text-purple-600 text-6xl font-bold uppercase">Music Recommender System</h1>
-                    <div className="text-theme-white text-xl py-5 text-center w-5/12">
-                        <p className={questionClasses}>What is this about?</p>
+                    <div className="text-theme-white text-xl py-5 text-center w-6/12">
+                        <p className={questionClasses}>&#10070; What is this about? &#10070;</p>
                         <p>A web-app that recommends you songs based on what songs you like.</p>
-                        <p className={questionClasses}>How this works?</p>
+                        <p className={questionClasses}>&#10070; How this works? &#10070;</p>
                         <p>You can start by searching for an artist/song and start liking.</p>
                         <p>Alternatively, you can also like random songs that the server provides you.</p>
                         <p>After liking a number of songs, you can access the recommendations page (by clicking on the "Get Recommendations" button), this page will fetch new songs recommended by the system based on your preferences.</p>
                         <p>Liking more songs will yield better results in the aspect of the accuracy.</p>
-                        <p className={questionClasses}>How to create an account?</p>
+                        <p className={questionClasses}>&#10070; How to create an account? &#10070;</p>
                         <p>You don't need to create an account!</p>
-                        <p>Your progress is saved locally automatically, to clear the history, use "Clear Preferences" button.</p>
-                        <p className={`${questionClasses} pb-0`}>Got it, where can I start?</p>
+                        <p>Your account is created automatically, the progress is saved both locally and in a mongoDB server.</p>
+                        <p>To clear your history, use "Clear Preferences" button.</p>
+                        <p className={`${questionClasses} pb-5`}>&#10070; Got it, where can I start? &#10070;</p>
+                        <StyledButton
+                            text="Get started"
+                            onClickEvent={() => history.push('/search')}
+                        />
                     </div>
-                    <StyledButton
-                        text="Get started"
-                        onClickEvent={() => history.push('/search')}
-                    />
                 </div>
             </Main>
         </>
